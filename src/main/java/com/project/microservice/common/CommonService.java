947ae90d1befb6b28a3a87327351a1e6e5c90b55
kseshasai <kseshasai@SYS-66.mshome.net>
2020-08-12 14:49:18 +0530

    initial commit

---
+++ src/main/java/com/project/microservice/common/CommonService.java
@@ -0,0 +1,13 @@
+package com.project.microservice.common;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonService {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/project/microservice/common/CommongServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.microservice.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommongServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
